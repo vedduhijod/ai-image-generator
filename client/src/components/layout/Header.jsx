8cf4fd1408e7779cb16e43cd50e668534d2da2e9
Ved Duhijod <vedduhijod@gmail.com>
2025-12-25 01:17:56 +0530

    ADD

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from "../../assets/logo.svg";
+
 
 function Header() {
   return (
     <div className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
   <Link to="/">
-    <img src="/logo.svg" alt="logo" className="w-28 object-contain" />
+    <img src={logo} alt="logo" className="w-10 object-contain" />
   </Link>
   <Link
     to="/create-post"
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
@@ -23,7 +23,7 @@ app.get("/", async (req, res) => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () =>
+    app.listen(PORT, () =>
       console.log("Server has started on port http://localhost:8080")
     );
   } catch (error) {
