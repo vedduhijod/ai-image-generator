01e78d5ac4e309c7b58cbf58b5ed7bdf8295a921
Ved Duhijod <vedduhijod@gmail.com>
2025-12-25 17:45:41 +0530

    Update frontend API URL for production

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,12 +20,16 @@ function Home() {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/v1/post`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
